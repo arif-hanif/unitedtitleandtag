c64de38a1b76d091052d36d1064d8ba2d0ae499f
Arif Hanif <arif.hanif3@gmail.com>
2020-04-19 15:14:55 -0400

    update site name

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,14 +1,13 @@
 module.exports = {
-    title: "United Title & Tag, LLC.",
-    description:
-      "A website for small business",
-    themeConfig: {
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'Services', link: '/services/' },
-        { text: 'Frequently Asked Questions', link: '/frequently-asked-questions/'},
-        { text: 'Forms', link: '/forms/' }
-      ],
-      docsDir: "src"
-    },
-  };
+  title: "United Title & Tag, Inc.",
+  description: "A website for small business",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Services", link: "/services/" },
+      { text: "Frequently Asked Questions", link: "/frequently-asked-questions/" },
+      { text: "Forms", link: "/forms/" },
+    ],
+    docsDir: "src",
+  },
+};
